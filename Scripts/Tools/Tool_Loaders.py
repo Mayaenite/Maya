c00f69614d223154dee3d6bacd616ec2a9f28ccc
Drew Loveridge <drew.loveridge@armstrong-white.com>
2018-08-17 13:12:37 -0400

    try statments added to AW Display Loader

--- Scripts/Tools/Tool_Loaders.py
+++ Scripts/Tools/Tool_Loaders.py
@@ -1,12 +1,14 @@
 import pymel.core as pm
-import  os
 
 def create_display_Layer_editor():
-	import Scripts.Tools.Display_Layer_Editor.Layer_Editor
 	try:
 		wig = Scripts.Tools.Display_Layer_Editor.Layer_Editor.Initialize_Layer_Editor()
 		return wig
-	except:
-		pass
+	except Exception as e:
+		print e
 	
-AW_Display_Layer_Editor = pm.evalDeferred(create_display_Layer_editor,lowestPriority=True)
+try:
+	import Scripts.Tools.Display_Layer_Editor.Layer_Editor
+	AW_Display_Layer_Editor = pm.evalDeferred(create_display_Layer_editor,lowestPriority=True)
+except Exception as e:
+	print e
