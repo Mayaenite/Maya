717ae38591d68b45c037c2c2cd7156d0a1e3d520
Drew Loveridge <mayaenite@hotmail.com>
2020-11-13 12:44:35 -0500

    Added a way to possibly load properly if AW_GLOBAL_SYSTEMS is not in environment

--- maya_initialization.py
+++ maya_initialization.py
@@ -6,7 +6,11 @@ import Scripts
 if os.environ.has_key("AW_GLOBAL_SYSTEMS"):
 	if not os.environ["AW_GLOBAL_SYSTEMS"] in os.sys.path:
 		os.sys.path.append(os.environ["AW_GLOBAL_SYSTEMS"])
-
+else:
+	this_dir = os.path.dirname(__file__)
+	global_systems_directory = os.path.realpath(this_dir+"/../../Global_Systems")
+	if os.path.exists(global_systems_directory) and not global_systems_directory in os.sys.path:
+		os.sys.path.append(global_systems_directory)
 from Environment_Access import System_Paths, System_Settings, utilities
 
 if System_Settings.USE_WING_DEBUG:
